29bbebb5b6eb57aea1f587c8c9c98a1fee0e14be
Juan De Leon <juaniscooldev@outlook.com>
2023-12-26 13:46:07 -0500

    Refactored Appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,17 +7,43 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          //upper title in home page
-          'Example Home Page',
-          style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: const Color.fromARGB(255, 14, 215, 95),
-        centerTitle: true,
+      appBar: appBar(),
+    );
+  }
 
-        leading: GestureDetector(
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        //upper title in home page
+        'Example Home Page',
+        style: TextStyle(
+            color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: const Color.fromARGB(255, 14, 215, 95),
+      centerTitle: true,
+
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          //container that contains an arrow
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 81, 241, 142), //box color
+              borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow_back.svg',
+            alignment: Alignment.center,
+
+            color: const Color.fromARGB(255, 4, 48, 27),
+            height: 30, //must define the width and height!!!!
+            width: 30,
+          ),
+        ),
+      ),
+
+      actions: [
+        GestureDetector(
           onTap: () {},
           child: Container(
             //container that contains an arrow
@@ -27,38 +53,15 @@ class MyHomePage extends StatelessWidget {
                 color: const Color.fromARGB(255, 81, 241, 142), //box color
                 borderRadius: BorderRadius.circular(10)),
             child: SvgPicture.asset(
-              'assets/icons/Arrow_back.svg',
+              'assets/icons/hamburger_menu.svg',
               alignment: Alignment.center,
-
               color: const Color.fromARGB(255, 4, 48, 27),
-              height: 30, //must define the width and height!!!!
               width: 30,
             ),
           ),
         ),
-
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              //container that contains an arrow
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 81, 241, 142), //box color
-                  borderRadius: BorderRadius.circular(10)),
-              child: SvgPicture.asset(
-                'assets/icons/hamburger_menu.svg',
-                alignment: Alignment.center,
-                color: const Color.fromARGB(255, 4, 48, 27),
-                width: 30,
-              ),
-            ),
-          ),
-        ],
-        elevation:
-            3.0, //shadow for appp (how high does it look from the screen)
-      ),
+      ],
+      elevation: 3.0, //shadow for appp (how high does it look from the screen)
     );
   }
 }
