360da5778b17f4b5c76fb62f3b6e7331b608517a
Juan De Leon <juaniscooldev@outlook.com>
2024-01-03 12:44:37 -0500

    Textbox, and listview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+//reference video: https://www.youtube.com/watch?v=D4nhaszNW4o
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -8,6 +10,90 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        searchField(),
+        const SizedBox(
+          height: 40,
+        ), //add some space from text field
+        const Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Category",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+        Container(
+          height: 150,
+          color: Colors.green,
+          child: ListView.builder(
+            //this will list all the items in the list
+            itemBuilder: (context, index) {
+              return Container();
+            },
+          ),
+        )
+      ]),
+    );
+  }
+
+  Container searchField() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: const Color(0xff1D1617).withOpacity(0.11), //shadow of
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white, //inside color of text box
+            contentPadding: const EdgeInsets.all(15),
+            hintText: "Type over here, ya dingus",
+            hintStyle: const TextStyle(
+              fontSize: 14,
+            ),
+            prefixIcon: const Padding(
+              padding: EdgeInsets.only(right: 1), // add padding to adjust icon
+              child: Icon(Icons.search_outlined),
+            ),
+            suffixIcon: Container(
+              width: 100, //must define width of container
+              child: const IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      //vertical line next to filter suffix icon
+                      color: Colors.black,
+                      indent: 10, //create space from top
+                      endIndent: 10, //create space from bottom
+
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Icon(Icons.tune_rounded),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius:
+                    BorderRadius.circular(15), //make text border rounded
+                borderSide: BorderSide.none)), //removes side border
+      ),
     );
   }
 
@@ -29,7 +115,7 @@ class MyHomePage extends StatelessWidget {
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
           decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 81, 241, 142), //box color
+              color: const Color.fromARGB(255, 68, 227, 129), //box color
               borderRadius: BorderRadius.circular(10)),
           child: SvgPicture.asset(
             'assets/icons/Arrow_back.svg',
